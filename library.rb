f53ebeff325053272ff47df7f918b4638cc9bd4f
GrejucAndrei <83236364+GrejucAndrei@users.noreply.github.com>
2022-08-27 19:24:49 +0300

    First Commit
    Added files

---
+++ library.rb
@@ -0,0 +1,30 @@
+class Book
+attr_accessor :title, :author, :pages, :genre
+end
+
+
+while true
+puts "\n[Enter a book title]"
+bookTitle = gets.chomp().to_s
+if bookTitle == "EXIT" or bookTitle == "exit"
+  break
+end
+puts "\n[Enter the book author]"
+bookAuthor = gets.chomp().to_s
+puts "\n[Enter the number of pages]"
+bookPages = gets.chomp().to_s
+puts "\n[Enter the genre of the book]"
+bookGenre = gets.chomp().to_s
+
+write_string_in_file = ("Title |" + bookTitle +
+  "| Author |" + bookAuthor + "| Pages |" + bookPages +
+  "| Genre |" + bookGenre + "| \n")
+
+  puts ("\n" + write_string_in_file + " was succesfully added. \n")
+  puts "\nType \"EXIT\" to close the script, or continue adding books"
+
+File.open("books", "a+") do |file|
+  file.write(write_string_in_file)
+end
+
+end
